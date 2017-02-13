3cc3c266f27391cabc27c06490b93f751eab194b
Andrew Ilijic <ailijic@gmail.com>
2017-02-13 13:00:22 -0500

    Unit 1.1.3

---
+++ js/components/person-list.js
@@ -0,0 +1,16 @@
+// js/components/person-list.js
+import React from "react";
+import Person from "./person";
+
+export default function PersonList() {
+    const people = [];
+    for (let i=0; i<5; i++) {
+        people.push(<Person />);
+    }
+
+    return (
+        <div className="person-list">
+            {people}
+        </div>
+    );
+}
